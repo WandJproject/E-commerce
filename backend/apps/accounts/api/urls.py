8f3fd4304ebb3cea2ee7d22d55d0b25a9756e66e
Alneelna <umeobiangela2@gmail.com>
2026-07-20 14:19:29 +0100

    feat: configure JWT authentication with Simple JWT

--- backend/apps/accounts/api/urls.py
+++ backend/apps/accounts/api/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import WelcomeAPIView
 
 urlpatterns = [
     path("", WelcomeAPIView.as_view(), name="api-home"),
+
+    path(
+        "auth/login/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+
+    path(
+        "auth/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
 ]
