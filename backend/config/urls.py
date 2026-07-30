0715eb9e5e13e5ae17234d00d8f6816396fecef2
Alneelna <umeobiangela2@gmail.com>
2026-07-30 10:53:42 +0100

    deployment

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home
 
 urlpatterns = [
+    path("", home),
+
     path("admin/", admin.site.urls),
+
     path("api/v1/", 
          include("apps.accounts.api.urls"),
     ),
@@ -55,4 +59,4 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
     )
-    
+
